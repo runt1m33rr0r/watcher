95cc345784f74ae19d775e9a8b6e72f7cb44a121
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-02-28 16:31:48 +0200

    realtime knn classification

--- watcher_camera/start.py
+++ watcher_camera/start.py
@@ -7,10 +7,22 @@ import pickle
 import os
 import os.path
 from face_recognition.face_recognition_cli import image_files_in_folder
+from PIL import Image
+import numpy
 
 
-def draw_box(frame, face_locations, face_names):
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
+def resize_image(image):
+    converted = Image.fromarray(image)
+    size = 1024, 1024
+
+    if converted.size[0] > size[0] or converted.size[1] > size[0]:
+        converted.thumbnail(size, Image.ANTIALIAS)
+
+    return numpy.array(converted)
+
+
+def draw_boxes(frame, prediction):
+    for name, (top, right, bottom, left) in prediction:
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 1
         right *= 1
@@ -30,11 +42,7 @@ def detect():
     video_capture = cv2.VideoCapture(0)
     classifier = train("images")
 
-    # image = face_recognition.load_image_file("images/ciri1.jpg")
-    # encoding = face_recognition.face_encodings(image)
-
-    face_locations = []
-    face_names = []
+    prediction = None
     process_this_frame = True
 
     while True:
@@ -46,25 +54,11 @@ def detect():
         converted_frame = frame[:, :, ::-1]
 
         if process_this_frame:
-            face_names = []
-
             prediction = predict(converted_frame, classifier)
-            print(prediction)
-
-            # face_locations = face_recognition.face_locations(converted_frame, model="cnn")
-            # face_encodings = face_recognition.face_encodings(converted_frame, face_locations)
-
-            # for face_encoding in face_encodings:
-            #     matches = face_recognition.compare_faces(encoding, face_encoding)
-
-            #     if True in matches:
-            #         face_names.append("ciri")
-            #     else:
-            #         face_names.append("unknown")
             
         process_this_frame = not process_this_frame
 
-        draw_box(frame, face_locations, face_names)
+        draw_boxes(frame, prediction)
 
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -86,11 +80,13 @@ def train(train_dir):
         # Loop through each training image for the current person
         for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
             image = face_recognition.load_image_file(img_path)
-            face_bounding_boxes = face_recognition.face_locations(image)
+            image = resize_image(image)
+            face_bounding_boxes = face_recognition.face_locations(image, model="cnn")
 
-            if len(face_bounding_boxes) != 1:
-                # If there are no people (or too many people) in a training image, skip the image.
-                print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+            if len(face_bounding_boxes) == 0:
+                print("No face in {}.".format(img_path))
+            elif len(face_bounding_boxes) > 1:
+                print("More than on face in {}.".format(img_path))
             else:
                 # Add face encoding for current image to the training set
                 X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
@@ -98,7 +94,7 @@ def train(train_dir):
 
     # Determine how many neighbors to use for weighting in the KNN classifier
     n_neighbors = int(round(math.sqrt(len(X))))
-    print("Chose n_neighbors automatically:", n_neighbors)
+    print("Chose n_neighbors automatically: ", n_neighbors)
 
     # Create and train the KNN classifier
     knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm="ball_tree", weights="distance")
@@ -113,10 +109,10 @@ def predict(image, knn_clf):
     if len(face_locations) == 0:
         return []
 
-    faces_encodings = face_recognition.face_encodings(image, known_face_locations=face_locations)
+    faces_encodings = face_recognition.face_encodings(image, face_locations)
 
     # Use the KNN model to find the best matches for the test face
-    distance_threshold = 0.6
+    distance_threshold = 0.55
     closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
     are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(face_locations))]
 
