0ce12dafc79eb17448b9a585a8505a512d8970ed
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-04-22 23:19:16 +0300

    basic user registration

--- watcher_server/server/models.py
+++ watcher_server/server/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Camera(models.Model):
+    url = models.CharField(max_length=1000)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    cameras = models.ManyToManyField(Camera)
--- watcher_server/server/views.py
+++ watcher_server/server/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 
 def index(request):
+    return render(request, 'base.html')
+
+
+def login(request):
     return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User.objects.create_user(username=username, email=None, password=password)
+        user.save()
+
+        print(User.objects.get(username='d'))
+
+    return render(request, 'register.html')
