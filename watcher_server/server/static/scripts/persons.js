a3538ecda201b40d7a6a1038f950a9f035b9150c
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-04-29 22:54:45 +0300

    persons page displays loaded image

--- watcher_server/server/static/scripts/persons.js
+++ watcher_server/server/static/scripts/persons.js
@@ -1,4 +1,23 @@
 (function() {
     'use strict';
+    
+    var validImageTypes = ['image/jpeg', 'image/png'];
+    var isValidFile = false;
 
+    $("#imageForm div input").on("input", function() {
+        var file = $(this).prop('files')[0];
+        var fileType = file["type"];
+
+        if (validImageTypes.includes(fileType)) {
+            isValidFile = true;
+
+            var fr = new FileReader();
+            fr.readAsDataURL(file);
+            fr.onload = function () {
+                $("#fileImage").attr("src", fr.result);
+            }
+        } else {
+            isValidFile = false;
+        }
+    });
 })();
