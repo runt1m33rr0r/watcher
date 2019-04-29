113faa2b9b3ce12f60c770f46485883780382ee5
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-04-29 14:03:50 +0300

    more reusable sidebar

--- watcher_server/server/static/scripts/persons.js
+++ watcher_server/server/static/scripts/persons.js
@@ -1,17 +1,4 @@
 (function() {
     'use strict';
 
-    $(".sidebar-search input").on("input", function() {
-        var searchedVal = $(this).val().toLowerCase();
-
-        $.each($("#sidebar-items .nav-link"), function() {
-            var currentVal = $(this).text().toLowerCase();
-
-            if (!currentVal.includes(searchedVal)) {
-                $(this).hide();
-            } else {
-                $(this).show();
-            }
-        });
-    });
 })();
--- watcher_server/server/static/scripts/sidebar.js
+++ watcher_server/server/static/scripts/sidebar.js
@@ -12,4 +12,18 @@
             open = true;
         }
     });
+
+    $(".sidebar-search input").on("input", function() {
+        var searchedVal = $(this).val().toLowerCase();
+
+        $.each($("#sidebar-items .nav-link"), function() {
+            var currentVal = $(this).text().toLowerCase();
+
+            if (!currentVal.includes(searchedVal)) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        });
+    });
 })();
