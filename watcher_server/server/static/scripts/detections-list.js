ad26ae807e92bc8ef024e8ec4dc062820867933e
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-05-10 00:27:47 +0300

    responisiveness fix

--- watcher_server/server/static/scripts/detections-list.js
+++ watcher_server/server/static/scripts/detections-list.js
@@ -1,6 +1,16 @@
 (function() {
     'use strict';
 
+    function adjustSize() {
+        var cardColumns = $('.card-columns');
+
+        if ($(window).width() <= 800){
+            cardColumns.css('width', '95%');
+        } else {
+            cardColumns.css('width', defaultWidth);
+        }
+    }
+
     var cards = $('.card');
     if (cards.length == 1) {
         var defaultWidth = '50%';
@@ -8,14 +18,7 @@
         $('.card-columns')
             .css('column-count', '1')
             .css('width', defaultWidth);
-
-        var cardColumns = $('.card-columns');
-        $(window).resize(function() {     
-            if ($(window).width() <= 800){
-                cardColumns.css('width', '95%');
-            } else {
-                cardColumns.css('width', defaultWidth);
-            }
-        });
+        adjustSize();
+        $(window).resize(adjustSize);
     }
 })();
