ca1134b766167ac7912af6ce1cf2f5cfdd0e8a11
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-05-11 19:27:57 +0300

    made javascript notification api

--- watcher_server/server/static/scripts/recognition.js
+++ watcher_server/server/static/scripts/recognition.js
@@ -1,6 +1,19 @@
 (function() {
     'use strict';
 
+    function notify(title, content) {
+        var notificationClone = $("#templateNotification").clone();
+        notificationClone.addClass("notification");
+        notificationClone.find(".toast-title").html(title);
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        notificationClone.find(".toast-time").html(time);
+        notificationClone.find(".toast-body").html(content);
+
+        $("#notifications").append(notificationClone);
+        $(".notification").toast("show");
+    }
+
     var fileImage = $("#fileImage");
     var processed = fileImage.attr("processed")
     if (processed != "") {
